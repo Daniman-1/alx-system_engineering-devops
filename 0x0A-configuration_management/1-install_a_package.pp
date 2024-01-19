15df55398cc2c50d18ea6ab86281a15266f923ec
Daniman-1 <danayarefayne@gmail.com>
2024-01-19 04:58:52 -0800

    updated

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-# Install puppet-lint
-package { 'Flask':
-  ensure          => '2.1.0',
-  provider        => 'pip3',
-  install_options => ['--force-reinstall'],
-}
+# install flask from pip3
+package { 'flask' :
+    ensure   => '2.1.0',
+    provider => 'pip3',
+    }
