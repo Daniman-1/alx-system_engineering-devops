b03165f4175353f1e3514aff86e8ac101d52e73a
Daniman-1 <danayarefayne@gmail.com>
2024-01-23 08:29:56 -0800

    updated

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,16 +1,16 @@
-file { '/etc/ssh/ssh_config':
-  ensure => file,
-  owner  => 'root',
-  group  => 'root',
-  mode   => '0644',
+# Seting up my client config file
+include stdlib
+
+file_line { 'Turn off passwd auth':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+  replace => true,
 }
 
-file_line { 'IdentityFile':
-  path => '/etc/ssh/ssh_config',
-  line => 'IdentityFile ~/.ssh/school',
-}
-
-file_line { 'PasswordAuthentication no':
-  path => '/etc/ssh/ssh_config',
-  line => 'PasswordAuthentication no',
+file_line { 'Delare identity file':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '     IdentityFile ~/.ssh/school',
+  replace => true,
 }
